8cc52e3f9e23986b266bc04623e6c8a3eb0b1690
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-04 19:38:52 -0400

    Code Challenge

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class BankAccount
+    {
+        public string Name;
+        public int ID;
+        public int Age;
+        public decimal Balance;
+        private decimal balance;
+        private double InterestRate;
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(string name, int id, decimal balance)
+        {
+            this.Name = name;
+            this.ID = id;
+            this.Balance = balance;
+        }
+
+        public decimal Balances
+        {
+            get { return balance; }
+        }
+
+        public void AddInterests()
+        {
+            balance += balance * (Decimal)InterestRate;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            balance -= amount;
+
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            balance += amount;
+        }
+    }
+}
---
+++ BankAccount/Transactions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Transactions
+    {
+        public decimal Amount { get; }
+
+        public Transactions(decimal amount)
+        {
+            this.Amount = amount;
+        }
+
+        public decimal Balances
+        {
+            get { return balance; }
+        }
+
+    }
+}
